484289e18a83c124dbab2d0309161a9b0f3ed425
Matic Jovan <matic.jovan219@gmail.com>
2026-05-12 16:22:50 +0200

    ft_atoi.c added

---
+++ lbft/ft_atoi.c
@@ -0,0 +1,48 @@
+#include "libft.h"
+
+int	ft_isspace(const char *ptr)
+{
+	int	i;
+
+	i = 0;
+	while (ptr[i] == ' '
+		|| (ptr[i] >= 9 && ptr [i] <= 13))
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_atoi(const char *ptr)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	sign = 0;
+	i = ft_isspace(ptr);
+	result = 0;
+	while (ptr[i] == '+' || ptr[i] == '-')
+	{
+		if (ptr[i] == '-')
+			sign++;
+		i++;
+	}
+	while (ptr[i] >= '0' && ptr[i] <= '9')
+	{
+		result = result * 10 + (ptr[i] - '0');
+		i++;
+	}
+	if (sign % 2 == 1)
+	{
+		sign = -1;
+		return (result * sign);
+	}
+	return (result);
+}
+/*
+int main(){
+	const char str[205] = "      \t \n -+-+2314";
+
+printf("%d", ft_atoi(str));
+}*/
--- lbft/libft.h
+++ lbft/libft.h
@@ -13,6 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdlib.h>
 # include <stddef.h>
 # include <stdio.h> //to be deleted after testing
 int	ft_isalpha(int i);

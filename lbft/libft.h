dbcc87c60ecbb700c509fb42ee8f1e2b1012111c
Matic Jovan <matic.jovan219@gmail.com>
2026-05-12 16:23:48 +0200

    ft_atoi and ft_calloc added to libft.h

--- lbft/libft.h
+++ lbft/libft.h
@@ -36,5 +36,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t n);
+int	ft_atoi(const char *ptr);
+void	*ft_calloc(size_t n, size_t size);
 
 #endif

58bddc62971115f2bb901a0c618d1a1d48a157b6
Jovan Matic <jomatic@student.42vienna.com>
2026-05-13 15:38:33 +0200

    trimming start is working but end not

--- lbft/ft_strtrim.c
+++ lbft/ft_strtrim.c
@@ -6,11 +6,54 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 13:09:32 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/13 14:43:07 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/13 15:38:00 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_find_start(const char *s1, const char *set)
+{
+	unsigned int	i;
+	int				j;
+	int				start;
+
+	i = 0;
+	j = 0;
+	start = 0;
+        while (set[j] != '\0')
+        {
+            if (set[j] == s1[i])
+            {
+				start++;
+            	i++;
+			}
+            j++;
+        }
+	return (start);
+}
+
+int	ft_find_end(const char *s1, const char *set)
+{
+	unsigned int	i;
+	int				j;
+	int				end;
+
+	i = ft_strlen(s1);
+	end = 0;
+	j = 0;
+		while ()////////
+		{
+			if (set[j] == s1[i])
+			{		
+				end++;
+				i--;
+				j = 0;
+			}
+			j++;
+		}
+	return (end);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -18,27 +61,17 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int	i;
 	int	j;
 	int	start;
+	int	end;
 	char *string;
 
 	i = 0;
 	j = 0;
 	start = 0;
+	end = 0;
 	string = malloc(ft_strlen(s1) * sizeof(char) + 1);
-	while (s1[i] != '\0')
-	{
-		while (set[j] != '\0')
-		{
-			if (set[j] == s1[i])
-			{
-			  start++;
-			}
-			i++;
-			j++;
-		}
-		j = 0;
-	}
-	ft_strlcpy(string, s1[start], ft_strlen(s1) + 1);
-	return (string);
+	start = ft_find_start(s1, set);
+	end = ft_find_end(s1, set);
+	return (ft_substr(s1, start, ft_strlen(s1) - (start + end)));
 }
 
 int main()

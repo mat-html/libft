5ad88cc85e138b5b26594f8df512d2316b75b9e2
Jovan Matic <jomatic@student.42vienna.com>
2026-05-14 12:57:54 +0200

    ft_strtrim.c done

--- lbft/ft_strtrim.c
+++ lbft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 13:09:32 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/13 15:38:00 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/14 12:57:21 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,64 +21,66 @@ int	ft_find_start(const char *s1, const char *set)
 	i = 0;
 	j = 0;
 	start = 0;
-        while (set[j] != '\0')
-        {
-            if (set[j] == s1[i])
-            {
-				start++;
-            	i++;
-			}
-            j++;
-        }
+	while (set[j] != '\0')
+	{
+		if (set[j] == s1[i])
+		{
+			start++;
+			i++;
+			j = 0;
+		}
+		j++;
+	}
 	return (start);
 }
 
 int	ft_find_end(const char *s1, const char *set)
 {
 	unsigned int	i;
-	int				j;
+	unsigned int	j;
 	int				end;
 
 	i = ft_strlen(s1);
 	end = 0;
 	j = 0;
-		while ()////////
+	while (i > 0 && j <= ft_strlen(set))
+	{
+		if (set[j] == s1[i])
 		{
-			if (set[j] == s1[i])
-			{		
-				end++;
-				i--;
-				j = 0;
-			}
-			j++;
+			end++;
+			i--;
+			j = 0;
 		}
+		j++;
+	}
 	return (end);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-
-	int	i;
-	int	j;
-	int	start;
-	int	end;
-	char *string;
+	int		i;
+	int		j;
+	int		start;
+	int		end;
+	char	*string;
 
 	i = 0;
 	j = 0;
 	start = 0;
 	end = 0;
 	string = malloc(ft_strlen(s1) * sizeof(char) + 1);
+	if (!string)
+		return (NULL);
 	start = ft_find_start(s1, set);
 	end = ft_find_end(s1, set);
 	return (ft_substr(s1, start, ft_strlen(s1) - (start + end)));
 }
-
+/*
 int main()
 {
-	char *s1 = "something will be deleted from the string something";
-	char *set = "something";
+	char *s1 = "";
+	char *set = "omething";
 
 	printf("%s", ft_strtrim(s1, set));
 					return (0);
-}
+}*/

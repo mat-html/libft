f5acdb74ac7d5af7d80e80eab021af6d454753b0
Jovan Matic <jomatic@student.42vienna.com>
2026-05-17 15:19:41 +0200

    ft_putnbr_fd done

--- lbft/ft_putnbr_fd.c
+++ lbft/ft_putnbr_fd.c
@@ -6,8 +6,21 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 14:17:10 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/16 14:17:11 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/17 15:19:06 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	*s;
+
+	s = ft_itoa(n);
+	ft_putstr_fd(s, fd);
+}
+
+int main()
+{
+	ft_putnbr_fd(-2147483648, 1);
+}

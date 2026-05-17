8aab934ffdd6cc35c40ef79654197d2c1aa72fbc
Jovan Matic <jomatic@student.42vienna.com>
2026-05-17 14:13:58 +0200

    ft_strmapi done

--- lbft/ft_strmapi.c
+++ lbft/ft_strmapi.c
@@ -6,8 +6,40 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 14:15:03 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/16 14:15:21 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/17 14:13:38 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	len;
+	unsigned int	i;
+
+	len = ft_strlen(s);
+	i = 0;
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*
+char add_index(unsigned int i, char c)
+{
+	return c + i;
+}
+
+int main()
+{
+	char *s = "aaaaaaaaaaaaaaaa";
+	
+	printf("%s", ft_strmapi(s, add_index));
+}*/

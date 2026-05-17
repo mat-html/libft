cddf2332f98d6f7a4346983cb567d5a2b8d2b30e
Jovan Matic <jomatic@student.42vienna.com>
2026-05-17 17:40:34 +0200

    Part 3 functions added to libft.h corrected typo

--- lbft/libft.h
+++ lbft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 14:47:36 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/17 17:20:23 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/17 17:40:18 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_lst **lst, t_lst *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));

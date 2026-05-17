8e9c8869af517ecec8f47f1733128142b2126b16
Jovan Matic <jomatic@student.42vienna.com>
2026-05-17 14:45:57 +0200

    ft_putchar_fd done

--- lbft/ft_putchar_fd.c
+++ lbft/ft_putchar_fd.c
@@ -6,8 +6,21 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 14:15:58 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/16 14:16:18 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/17 14:45:07 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+/*
+int main()
+{
+	char c = 'c';
+	int fd = 0;
+
+	ft_putchar_fd(c, fd);
+}*/

f6f5148b128e1d0d8500801ad6f2c9bc1defb257
Jovan Matic <jomatic@student.42vienna.com>
2026-05-17 15:14:50 +0200

    ft_putendl_fd done

--- lbft/ft_putendl_fd.c
+++ lbft/ft_putendl_fd.c
@@ -6,8 +6,28 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 14:16:58 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/16 14:17:00 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/17 15:14:14 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+/*
+int main()
+{
+	ft_putendl_fd("something", 1);
+	ft_putendl_fd("something", 1);
+	return (0);
+}*/

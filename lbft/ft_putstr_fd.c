fb45d7e4b05a5a6ab16f719334e0719d22e21a6d
Jovan Matic <jomatic@student.42vienna.com>
2026-05-17 14:51:54 +0200

    ft_putstr_fd done

--- lbft/ft_putstr_fd.c
+++ lbft/ft_putstr_fd.c
@@ -6,8 +6,30 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 14:16:43 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/16 14:16:46 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/17 14:51:29 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+/*
+int main()
+{
+	char *s = "something";
+
+	ft_putstr_fd(s, 1);
+	ft_putstr_fd(s, 1);
+	return (0);
+}*/
